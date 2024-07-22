d782d08920ad1f64b2ca481edde6e39e5749c7af
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-22 10:52:07 -0700

    implement `classes.json` data

--- main.ts
+++ main.ts
@@ -1,21 +1,44 @@
+import type { ExtractSchema } from "@orama/orama";
 import { create, insert, search } from "@orama/orama";
+import CLASS_DATA from "./classes.json" with { type: "json" };
 import PROPERTY_DATA from "./properties.json" with { type: "json" };
 
-const db = await create({
+export type PropertyDocument = ExtractSchema<typeof PROPERTY_SCHEMA>;
+
+export const PROPERTY_SCHEMA = {
   schema: {
     itemID: "string",
     label: "string",
     comment: "string",
   },
-});
+} as const;
 
+export type ClassDocument = ExtractSchema<typeof CLASS_SCHEMA>;
+
+export const CLASS_SCHEMA = {
+  schema: {
+    ...PROPERTY_SCHEMA.schema,
+    properties: "string[]",
+  },
+} as const;
+
+const propertyDb = await create(PROPERTY_SCHEMA);
 for (const doc of PROPERTY_DATA) {
-  await insert(db, doc as never);
+  await insert(propertyDb, doc as never);
 }
 
-const searchResult = await search(db, {
-  term: "movie",
-});
-console.log(searchResult);
+const classDb = await create(CLASS_SCHEMA);
+for (const doc of CLASS_DATA) {
+  await insert(classDb, doc as never);
+}
 
-// TODO: Separate example searching for classes.
+const propertyResult = await search(
+  propertyDb,
+  { term: "movie" },
+);
+const classResult = await search(
+  classDb,
+  { term: "movie" },
+);
+
+console.log({ propertyResult, classResult });
